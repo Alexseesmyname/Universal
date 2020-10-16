c515abbef36b368785b96538df561f4346f89bdd
Alexseesmyname <skalpel550@gmail.com>
2020-10-16 16:44:51 +0300

    Added article.html, fixed index.html

--- js/main.js
+++ js/main.js
@@ -1,7 +1,17 @@
-var mySwiper = new Swiper('.swiper-container', {
+const marks = document.querySelectorAll("#bookmark");
+for(let mark of marks){
+  mark.addEventListener("click", function(event){
+    mark.classList.toggle("item__icon_active");
+  })
+};
+
+var mySwiper = new Swiper('.swiper-slider__container', {
   // Optional parameters
   direction: 'horizontal',
   loop: true,
+  autoplay: {
+    delay: 3000,
+  },
 
   // If we need pagination
   pagination: {
