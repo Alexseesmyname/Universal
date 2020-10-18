833f14c1fe2836512892f18fd847a7c836d90cba
Alexseesmyname <skalpel550@gmail.com>
2020-10-18 14:18:50 +0300

    Added taps

--- js/main.js
+++ js/main.js
@@ -1,3 +1,30 @@
+//Тапы
+const tapButtons = document.querySelectorAll(".tap");
+const tapItems = document.querySelectorAll(".header-item");
+
+tapButtons.forEach(function(tap){
+  tap.addEventListener("click", function(){
+    let tapId = tap.getAttribute("data-tap");
+    let activeTap = document.querySelector(tapId);
+
+    if( ! tap.classList.contains("tap_active") ) {
+      tapButtons.forEach(function(tap){
+        tap.classList.remove("tap_active");
+      });
+      tapItems.forEach(function(tapItem){
+        tapItem.classList.remove("header-item_active");
+      });
+  
+      tap.classList.add("tap_active");
+      activeTap.classList.add("header-item_active");
+
+    };
+
+  });
+});
+document.querySelector(".tap").click();
+
+//Смена цвета у закладок
 const marks = document.querySelectorAll("#bookmark");
 for(let mark of marks){
   mark.addEventListener("click", function(event){
@@ -5,6 +32,7 @@ for(let mark of marks){
   })
 };
 
+//Слайдер в мейне
 var mySwiper = new Swiper('.swiper-slider__container', {
   // Optional parameters
   direction: 'horizontal',
@@ -13,7 +41,6 @@ var mySwiper = new Swiper('.swiper-slider__container', {
     delay: 3000,
   },
 
-  // If we need pagination
   pagination: {
     el: '.swiper-pagination',
     bulletClass: "slider-bullet",
@@ -24,6 +51,7 @@ var mySwiper = new Swiper('.swiper-slider__container', {
 
 });
 
+//Слайдер в статье
 var mySwiper = new Swiper('.article-swiper', {
   // Optional parameters
   direction: 'horizontal',
